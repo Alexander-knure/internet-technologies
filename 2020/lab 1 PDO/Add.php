495134a6bd8a2c3740007179e5d2157ca7a412cb
Alex <oleksandr.tereshchenko@nure.ua>
2020-03-27 15:27:51 +0200

    fix Add.php

--- 2020/lab 1 PDO/Add.php
+++ 2020/lab 1 PDO/Add.php
@@ -1,10 +1,10 @@
- <?php
+<?php
 include 'Connect.php';
 $dbh->beginTransaction();
 
 $res0=$dbh->query("SELECT COUNT(*) from lesson");
 $count=$res0 ->fetchColumn();
-//echo $count;
+$count++;
 
 $q = "INSERT INTO lesson SET
 ID_Lesson='".$count."',
@@ -13,37 +13,26 @@ lesson_number='".$_POST['numberL']."',
 auditorium='".$_POST['audN']."',
 disciple='".$_POST['disciple']."',
 type='Practical'";
+echo $q."<br>";
+$dbh->query($q);
 
-//$dbh->query($q);
-echo $q;
-//print_r($_POST);
-//echo $_POST['disciple'];
-
-/*$dbh->query("INSERT INTO lesson SET
-ID_Lesson='".$count."',
-week_day='".$_POST['week']."',
-lesson_number='".$_POST['numberL']."',
-auditorium='".$_POST['audN']."',
-disciple='".$_POST[`nameD`]."',
-type='Practical'");*/
-
-
-$dbh->query("INSERT INTO lesson_groups SET 
+$q = "INSERT INTO lesson_groups SET
 FID_Lesson2={$count},
-FID_Groups={$_POST['group']}");
+FID_Groups={$_POST['group']}";
+echo $q."<br>";
+$dbh->query($q);
 
-$dbh->query("INSERT INTO lesson_teacher SET 
+$q = "INSERT INTO lesson_teacher SET
 FID_Teacher={$_POST['teacher']},
-FID_Lesson1={$count}");
+FID_Lesson1={$count}";
+echo $q."<br>";
+$dbh->query($q);
 
 $isOK = $dbh->commit();
 if($isOK != 0)
-{
     echo '<br>New row added';
-}
 else
-{
     echo '<br>Error in transaction';
-}
+
 echo '<br><a href="MainPage.php">Back</a>';
 ?>
